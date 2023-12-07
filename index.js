b23f7cacf6ed84ed35a7cfe0c0c6d7418b7194be
V SHANMUKHA RAJU <69906555+vsraju1@users.noreply.github.com>
2023-12-07 15:48:48 +0000

    Loading animation is done

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
-alert("Hi V Shanmukha raju")
+function videoAnimation() {
+    let videocon = document.querySelector(".main__videoContainer")
+    let playCurser = document.querySelector(".main__curser")
+
+    videocon.addEventListener("mouseenter", () => {
+        gsap.to(playCurser, {
+            scale: 0.7,
+            opacity: 1
+        })
+    })
+
+    videocon.addEventListener("mouseleave", () => {
+        gsap.to(playCurser, {
+            scale: 0,
+            opacity: 0
+        })
+    })
+
+    videocon.addEventListener("mousemove", (dets) => {
+        gsap.to(playCurser, {
+            left: dets.x - 50,
+            top: dets.y - 40
+        })
+    })
+}
+
+videoAnimation()
+
+
+function loadingAnimation() {
+    gsap.from(".main__pageOne h1", {
+        opacity: 0,
+        y: 100,
+        delay: 0.5,
+        duration: 0.9,
+        stagger: 0.3
+    })
+    gsap.from(".main__videoContainer", {
+        opacity: 0,
+        scale:0.9,
+        delay: 1.2,
+        duration: 0.5,
+        
+    })
+}
+
+loadingAnimation()
+
